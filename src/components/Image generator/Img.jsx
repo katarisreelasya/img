6bf9affd694d595a48d1c7d634df6e8a59920d68
katarisreelasya25@gmail.com <katarisreelasya25@gmail.com>
2024-04-25 11:17:20 +0530

    navbar

--- src/components/Image generator/Img.jsx
+++ src/components/Image generator/Img.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useRef } from 'react';
 
 const Img = () => {
-    const [imageBase64, setImageBase64] = useState(''); // State to store base64 string
+    const [imageBase64, setImageBase64] = useState('');
+    const [loading, setLoading] = useState(false); // Added loading state
     const [errorMessage, setErrorMessage] = useState('');
-
     let inpRef = useRef(null);
 
     const Search = async () => {
@@ -13,6 +13,7 @@ const Img = () => {
         }
 
         setErrorMessage('');
+        setLoading(true); // Set loading to true when starting the fetch
 
         try {
             const res = await fetch('https://imageai-generator.p.rapidapi.com/image', {
@@ -37,18 +38,19 @@ const Img = () => {
 
             const data = await res.json();
             const base64String = data.body; 
-            console.log(data);
             setImageBase64(data);
         } catch (error) {
             console.error('Error:', error);
             setErrorMessage('An error occurred. Please try again later.');
+        } finally {
+            setLoading(false); // Set loading to false after fetch is done
         }
     }
 
     return (
         <div>
-            <div id='navbar'>
-            <nav class="bg-white border-gray-200 dark:bg-gray-900">
+            <div id="navbar">
+      <nav class="bg-white border-gray-200 dark:bg-gray-900">
                 <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                   <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
                       <img src="logo1.jpg" class="h-8" alt="Flowbite Logo" />
@@ -75,28 +77,30 @@ const Img = () => {
                   </div>
                 </div>
               </nav>
-            </div>
+            </div>  
             <section className="bg-gradient-to-b from-pink-200 via-pink-100 to-yellow-50 w-screen h-screen overflow-x-hidden">
-            <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative flex flex-col items-center justify-center">
-                <h1 className='font-bold text-5xl font-sans text-black mb-6'><span className='font-bold text-5xl text-pink-600'>Pixel</span>Craft</h1>
-                <h1 className="mb-4 text-2xl font-bold font-sans tracking-tight leading-none text-gray-700 md:text-3xl lg:text-4xl ">Crafting Creativity with Every Pixel</h1>
-                <p className="mb-8 text-lg font-normal text-black lg:text-xl sm:px-16 lg:px-48">Welcome to ImageForge, your creative hub for stunning visuals. Our React-based project empowers you to craft captivating images effortlessly. With a user-friendly interface and customizable options, designing eye-catching graphics is a breeze. Say goodbye to stock photos and hello to personalized imagery. Explore endless possibilities with ImageForge!</p>
-                <div className="w-[80vw] mx-auto flex flex-col items-center justify-center">   
-                    <label htmlFor="default-email" className="mb-2 text-sm font-medium text-gray-800 sr-only">Enter Prompt : </label>
-                    <div className="relative mb-8 flex flex-col items-center w-[25vw] justify-center">
-                        <div className="absolute inset-y-0  rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                            <h6>&#10024;</h6>
+                <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 z-10 relative flex flex-col items-center justify-center">
+                    <h1 className='font-bold text-5xl font-sans text-black mb-6'><span className='font-bold text-5xl text-pink-600'>Pixel</span>Craft</h1>
+                    <h1 className="mb-4 text-2xl font-bold font-sans tracking-tight leading-none text-gray-700 md:text-3xl lg:text-4xl ">Crafting Creativity with Every Pixel</h1>
+                    <p className="mb-8 text-lg font-normal text-black lg:text-xl sm:px-16 lg:px-48">Welcome to ImageForge, your creative hub for stunning visuals. Our React-based project empowers you to craft captivating images effortlessly. With a user-friendly interface and customizable options, designing eye-catching graphics is a breeze. Say goodbye to stock photos and hello to personalized imagery. Explore endless possibilities with ImageForge!</p>
+                    <div className="w-[80vw] mx-auto flex flex-col items-center justify-center">   
+                        <label htmlFor="default-email" className="mb-2 text-sm font-medium text-gray-800 sr-only">Enter Prompt : </label>
+                        <div className="relative mb-8 flex flex-col items-center w-[25vw] justify-center">
+                            <div className="absolute inset-y-0  rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                                <h6>&#10024;</h6>
+                            </div>
+                            <input type="text" ref={inpRef} id="default-text" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-double border-pink-700 rounded-full bg-white placeholder:text-gray-500 " placeholder="Imagine Wonders....." required />
+                            <button onClick={Search} className="absolute end-2.5 bottom-2.5 bg-gradient-to-r from-pink-400 to to-yellow-500 hover:bg-blue-800 focus:ring-4 focus:outline-none  rounded-full text-black text-sm font-semibold px-4 py-2 ">Generate</button>
                         </div>
-                        <input type="text" ref={inpRef} id="default-text" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-double border-pink-700 rounded-full bg-white placeholder:text-gray-500 " placeholder="Imagine Wonders....." required />
-                        <button onClick={Search} className="absolute end-2.5 bottom-2.5 bg-gradient-to-r from-pink-400 to to-yellow-500 hover:bg-blue-800 focus:ring-4 focus:outline-none  rounded-full text-black text-sm font-semibold px-4 py-2 ">Generate</button>
+                        <div className='w-[60vw] h-[30vh] mb-3  flex flex-col items-center justify-around border-pink-700 rounded-lg py-2 px-3 '>
+                            {loading ? 
+                                    <div class="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">loading...</div>
+                                : (imageBase64 && <img width={215} height={215} src={`data:image/jpeg;base64,${imageBase64}`} alt="Generated Image" className='rounded-lg' />)}
+                        </div>
+                        <div>{errorMessage && <p className="text-red-600">{errorMessage}</p>}</div>
                     </div>
-                    <div className='w-[60vw] h-[30vh] mb-3  flex flex-col items-center justify-around border-pink-700 rounded-lg py-2 px-3 '>
-                    {imageBase64 && <img width={215} height={215} src={`data:image/jpeg;base64,${imageBase64}`} alt="Generated Image" className='rounded-lg' />}
-                    </div>
-                    <div>{errorMessage && <p className="text-red-600">{errorMessage}</p>}</div>
                 </div>
-            </div>
-        </section>
+            </section>
         </div>    
     );
 };
