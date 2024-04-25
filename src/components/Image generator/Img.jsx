c3ae9f2df92cb88f872b19f566eec2b32dd7e372
katari sree lasya <112769078+katarisreelasya@users.noreply.github.com>
2024-04-25 08:37:51 +0530

    Update Img.jsx

--- src/components/Image generator/Img.jsx
+++ src/components/Image generator/Img.jsx
@@ -57,9 +57,11 @@ const Img = () => {
                         <div className="absolute inset-y-0  rtl:inset-x-0 start-0 flex items-center ps-3.5 pointer-events-none">
                             <h6>&#10024;</h6>
                         </div>
-                        <input type="text" ref={inpRef} id="default-text" className="block w-[80vw] sm:w-full md:w-full lg:w-full  p-4 ps-10 text-sm text-gray-900 border border-double border-pink-700 rounded-full bg-white placeholder:text-gray-500 " placeholder="Imagine Wonders....." required />
-                        <button onClick={Search} className="absolute end-2.5 bottom-2.5 bg-gradient-to-r from-pink-400 to to-yellow-500 hover:bg-blue-800 focus:ring-4 focus:outline-none  rounded-full text-black text-sm font-semibold px-4 py-2 ">Generate</button>
-                    </div>
+                        <div className="w-[80vw] sm:w-full md:w-full lg:w-full h-auto">
+                        <input type="text" ref={inpRef} id="default-text" className="block flex items-center justify-self-end w-[80vw] sm:w-full md:w-full lg:w-full  p-4 ps-10 text-sm text-gray-900 border border-double border-pink-700 rounded-full bg-white placeholder:text-gray-500 " placeholder="Imagine Wonders....." required />
+                        <button onClick={Search} className=" bg-gradient-to-r from-pink-400 to to-yellow-500 hover:bg-blue-800 focus:ring-4 focus:outline-none  rounded-full text-black text-sm font-semibold px-4 py-2 ">Generate</button>
+                         </div>
+                        </div>
                     <div className='w-[60vw] h-[30vh] mb-3 border flex flex-col items-center justify-around border-pink-700 rounded-lg py-2 px-3 '>
                     {imageBase64 && <img width={215} height={215} src={`data:image/jpeg;base64,${imageBase64}`} alt="Generated Image" className='rounded-lg' />}
                     </div>
